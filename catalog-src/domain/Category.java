5b0b9fd45f45776b6ce93a4c50489875a9549fb8
Sergey Yermochenko <yermochenko@gmail.com>
2016-09-28 00:33:04 +0300

    доменные объекты создана модель предметной области

---
+++ catalog-src/domain/Category.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.List;
+
+public class Category {
+	private Integer id;
+	private String name;
+	private Category parent;
+	private List<Category> children;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getParent() {
+		return parent;
+	}
+
+	public void setParent(Category parent) {
+		this.parent = parent;
+	}
+
+	public List<Category> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<Category> children) {
+		this.children = children;
+	}
+}
---
+++ catalog-src/domain/Product.java
@@ -0,0 +1,49 @@
+package domain;
+
+public class Product {
+	private Integer id;
+	private String name;
+	private String description;
+	private Integer price;
+	private Category category;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+}
