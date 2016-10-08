d9bfbb008865565448d9d3f0a7d20c6a9e3878b4
Sergey Yermochenko <yermochenko@gmail.com>
2016-10-08 13:25:03 +0300

    рефакторинг убраны неиспользуемые конструкторы

--- catalog-src/dao/DaoException.java
+++ catalog-src/dao/DaoException.java
@@ -1,16 +1,6 @@
 package dao;
 
 public class DaoException extends Exception {
-	public DaoException() {}
-
-	public DaoException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public DaoException(String message) {
-		super(message);
-	}
-
 	public DaoException(Throwable cause) {
 		super(cause);
 	}
